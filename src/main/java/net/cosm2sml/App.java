9be99e68ebddb8bf0f1d45c7e661467f6429a8a8
Moh-Yakoub <moh.ahmed.hamed@gmail.com>
2013-05-14 01:49:27 +0200

    initial stage of encoder

--- src/main/java/net/cosm2sml/App.java
+++ src/main/java/net/cosm2sml/App.java
@@ -1,7 +1,21 @@
 package net.cosm2sml;
 
+import java.util.List;
+
 import net.cosm2sml.helper.impl.CosmFeedHelperImpl;
 import net.cosm2sml.sensorml.sensorML;
+import net.cosm2sml.util.Constants;
+import net.opengis.sensorML.x101.IdentificationDocument.Identification;
+import net.opengis.sensorML.x101.IdentificationDocument.Identification.IdentifierList;
+import net.opengis.sensorML.x101.IdentificationDocument.Identification.IdentifierList.Identifier;
+import net.opengis.sensorML.x101.KeywordsDocument.Keywords;
+import net.opengis.sensorML.x101.KeywordsDocument.Keywords.KeywordList;
+import net.opengis.sensorML.x101.SensorMLDocument;
+import net.opengis.sensorML.x101.SensorMLDocument.SensorML;
+import net.opengis.sensorML.x101.TermDocument;
+import net.opengis.sensorML.x101.TermDocument.Term;
+import net.opengis.sensorML.x101.impl.SensorMLDocumentImpl;
+import net.opengis.sensorML.x101.impl.SensorMLDocumentImpl.SensorMLImpl;
 
 /**
  * Hello world!
@@ -13,6 +27,40 @@ public class App {
 		CosmFeed feed = helper.parseFeed(126768);
 		sensorML ml = helper.decodeFeed(feed);
 		// use ml
+
+		SensorML sensor = SensorML.Factory.newInstance();
+		/*
+		 * Add keywords
+		 */
+		Keywords keywords = sensor.addNewKeywords();
+		KeywordList keywords_list = keywords.addNewKeywordList();
+
+		List<String> feed_keywords = ml.getKeywords();
+		for(String s:feed_keywords)
+			keywords_list.addKeyword(s);
+		
+		
+		
+		
+		Identification identification = sensor.addNewIdentification();
+		/*
+		 * Identification list
+		 */
+		IdentifierList list = identification.addNewIdentifierList();
+		Identifier longname = list.addNewIdentifier();
+		Term t = longname.addNewTerm();
+		t.setDefinition(Constants.LONG_NAME_SCHEMA);
+		t.setValue(ml.getLong_name());
+
+		/*
+		 * Short name identifier
+		 */
+		Identifier shortname = list.addNewIdentifier();
+		t = shortname.addNewTerm();
+		t.setDefinition(Constants.SHORT_NAME_SCHEMA);
+		t.setValue(ml.getShort_name());
+
+		System.out.println(sensor.toString());
 
 	}
 }
--- src/main/java/net/cosm2sml/util/Constants.java
+++ src/main/java/net/cosm2sml/util/Constants.java
@@ -1,5 +1,8 @@
 package net.cosm2sml.util;
 
 public class Constants {
-	public static final String API_KEY = "api_key_goes_here";
+	public static final String API_KEY = "YEj6769_jSONmqPYnEBsmrx_z7-SAKx2RGZaV0dDWU0wdz0g";
+	public static final String LONG_NAME_SCHEMA = "urn:ogc:def:identifier:OGC:longname";
+	public static final String SHORT_NAME_SCHEMA = "urn:ogc:def:identifier:OGC:shortname";
+	public static final String KEYWORDS_SCHEMA = "urn:x-nasa:def:gcmd:keywords";
 }
